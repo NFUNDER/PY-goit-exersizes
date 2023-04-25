ec254a3f0512f9c6bcc7e206646442568803d083
NFUNDER <funder@bigmir.net>
2023-04-25 13:38:11 +0300

    week1

---
+++ stage_1week.py
@@ -0,0 +1,56 @@
+# import math
+# x = 8**3 + 4*(2+2)
+# print(x)
+# namec = input("Enter your name: ")
+# # print(namec)
+# num_1 = 90
+# num_2 = 87
+# exampe = f"Simple example {num_1} + {num_2} = {num_1 + num_2}"
+# print(exampe)
+
+# name = input("Enter your name: ")
+# last_name = input('Enter your last name:')
+# full_name = f"{name} {last_name}"
+# print(full_name)
+
+
+#ТИПИ ДАНИХ 'convertation'
+# num_1 = str(1) # integer
+# num_2 = int(float(str(1.8))) #float
+# string_1 = int('3')
+# print(type(num_1), type(num_2), type(string_1))
+# 
+
+# volume = float(input('Fuel volume: '))
+# price = float(input('Fuel price: '))
+# bill = f'Your bill is {volume * price}'
+# print(bill)
+# print(type(volume), type(price))
+
+# length = float('2.75')
+# width = float("1.75")
+# area = length * width
+# print(area)
+
+# length = float("2.75")
+# width = float("1.75")
+# area = length * width
+# show = f"With width {float(2.75)}, and length {float(1.75)} of the room,ow its area is equal to {(length * width)}"
+# # show = f"With width {"2.75"}, and length {"1.75"} of the room, its area is equal to {float(length) * float(width)}"
+# print(show)
+
+
+# import math
+
+# RADIUS = 6371.01
+
+# lat1 = 50.45
+# lon1 = 30.523
+
+# lat2 = 51.5072
+# lon2 = -0.1275
+
+# distance = RADIUS * math.acos(math.sin(math.radians(lat1))) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.cos(math.radians(lon1) - math.radians(lon2))
+# print(distance)
+
+rom math import sqrt
